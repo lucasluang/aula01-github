be782f8ee2a79021a550e7a9a079b6bb91e602a1
lucasluang <lucasluang@yahoo.com.br>
2020-02-03 13:15:09 -0300

    Acrescentada  soma e for

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,17 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int x = 10;
+		int y = 20;
+		
+		int soma = x+y;
+		
+		System.out.println(soma);
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Teste!");
+		}
 
 	}
 
